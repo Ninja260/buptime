42b2a1714f05d6f67e442f54a909b6eb95553c60
Myo Min Hein <myominhein.irrahub@gmail.com>
2024-12-13 18:10:51 +0700

    add autostart on first login

--- install.sh
+++ install.sh
@@ -2,6 +2,10 @@
 
 CWD="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
 
+# make empty /var/log/on_unplugged.log file
+sudo touch /var/log/on_unplugged.log
+sudo chmod 777 /var/log/on_unplugged.log
+
 # move script files to /etc/scripts/buptime
 [ -e /etc/scripts/buptime ] || sudo mkdir -p /etc/scripts/buptime
 sudo cp $CWD/scripts/*.sh /etc/scripts/buptime
@@ -9,7 +13,11 @@ sudo cp $CWD/scripts/*.sh /etc/scripts/buptime
 # move on_unplugged.rules file to /etc/udev/rules.d/
 sudo cp $CWD/scripts/power_unplugged.rules /etc/udev/rules.d/power_unplugged.rules
 
-# make symmetric link of buptime.sh to /usr/bin/buptime
+# make symmetric links
 sudo ln -s /etc/scripts/buptime/buptime.sh /usr/bin/buptime
+sudo ln -s /etc/scripts/buptime/on_unplugged.sh /usr/bin/on_unplugged.sh
+
+# Add auto start on frist login file
+sudo cp $CWD/scripts/buptime.desktop ~/.config/autostart/buptime.desktop
 
 echo "Installation successful!"
--- uninstall.sh
+++ uninstall.sh
@@ -2,13 +2,20 @@
 
 CWD="$(cd "$(dirname "$(realpath "${BASH_SOURCE[0]}")")" && pwd)"
 
+# remove auto start on first login file
+sudo rm ~/.config/autostart/buptime.desktop
+
 # remove symmetric link of /usr/bin/buptime
 sudo rm /usr/bin/buptime
+sudo rm /usr/bin/on_unplugged.sh
 
 # remove on_unplugged.rules files from /etc/udev/rules.d/
 sudo rm /etc/udev/rules.d/power_unplugged.rules
 
 # remove "buptime" folder from /etc/scripts folder
 sudo rm -r /etc/scripts/buptime/
+
+# remove /var/log/on_unplugged.log file
+sudo rm /var/log/on_unplugged.log
 
 echo "Uninstallation successful!"

9b9178aae92d5c40fc0577c42242ad29a1ce5729
Myo Min Hein <myominhein.irrahub@gmail.com>
2026-02-27 11:46:30 +0700

    optimize get current battery percentage script

--- scripts/current_battery_percentage.sh
+++ scripts/current_battery_percentage.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
 
-output=$(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage:)
-output=${output//"percentage:"/}
-
-# trim whitespaces
-battery_percentage=$(echo "$output" | xargs)
-battery_percentage="${battery_percentage%%%}"
-
-echo $battery_percentage
+cat /sys/class/power_supply/BAT0/capacity
